88fa4da313ce7e8e39014d43ab2d6a50e32891b7
Patrick Little <little.patrick@outlook.com>
2024-12-11 09:44:03 -0700

    team class done

--- lib/team.rb
+++ lib/team.rb
@@ -1,2 +1,19 @@
 require './lib/player.rb'
 
+class Team 
+  attr_accessor :mascot, :city, :roster
+
+  def initialize(mascot, city)
+    @mascot = mascot
+    @city = city
+    @roster = []
+  end
+
+  def add_player(player)
+    @roster << player
+  end
+    
+  def player_count
+    @roster.length
+  end
+end
