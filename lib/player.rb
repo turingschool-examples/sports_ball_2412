01fb62ae6278fea3e3c0a1002d824d84af622020
Will Fox <william.macdonald.fox@gmail.com>
2024-12-11 13:27:48 -0500

    feat: add set_nickname! method, complete iteration 3

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,13 @@
 require 'pry'
 
 class Player 
-    attr_reader :name, :monthly_cost, :contract_length
+    attr_reader :name, :monthly_cost, :contract_length, :nickname
 
     def initialize(name, monthly_cost, contract_length)
         @name = name
         @monthly_cost = monthly_cost
         @contract_length = contract_length
+        @nickname = nil
     end
 
     def first_name
@@ -19,5 +20,9 @@ class Player
 
     def total_cost
         @monthly_cost * @contract_length 
+    end
+
+    def set_nickname!(nick_name)
+        @nickname = nick_name
     end
 end
