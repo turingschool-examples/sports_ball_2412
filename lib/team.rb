247206057411582d7867529eb79a16655e3f45f9
Sebastian McKee <sebmcke@gmail.com>
2024-12-11 10:27:59 -0600

    Add player_count and add_player methods to pass iteration 2 tests

--- lib/team.rb
+++ lib/team.rb
@@ -1,6 +1,15 @@
 class Team
+    attr_reader :roster, :player_count
 
     def initialize(name, city)
-        
+        @roster = []
+    end
+
+    def player_count
+        @roster.size
+    end
+
+    def add_player(player)
+        @roster << player
     end
 end
