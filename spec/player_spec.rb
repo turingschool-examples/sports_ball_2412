fe42fe512de443c3447a5f6100d56e7acac59307
Sebastian McKee <sebmcke@gmail.com>
2024-12-11 10:44:45 -0600

    Add iteration 3 tests to player spec class

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,18 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+  it 'does not have a nickname by default' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    expect(player.nickname).to eq(nil)
+  end
+
+  it 'can have a nickname' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    player.set_nickname("Squints")
+
+    expect(player.nickname).to eq("Squints")
+  end
 end
