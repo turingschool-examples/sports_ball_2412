48afe8205ca5aab6bb67790887faf41e73d71a9c
Sebastian McKee <sebmcke@gmail.com>
2024-12-11 10:17:34 -0600

    Add total_cost variable to Player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,13 @@
 class Player
-    attr_reader :first_name, :last_name, :monthly_cost, :contract_length
+    attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :total_cost
 
     def initialize(name, monthly_cost, contract_length)
         @monthly_cost = monthly_cost
         @contract_length = contract_length
+        @total_cost = monthly_cost * contract_length
 
         names = name.split(' ')
         @first_name = names.first
         @last_name = names.last
-        binding.pry
     end
 end
