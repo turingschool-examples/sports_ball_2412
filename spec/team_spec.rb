58cacdf9f5da4acb40507918c0dbe6beee82d77e
Sebastian McKee <sebmcke@gmail.com>
2024-12-11 10:38:56 -0600

    Change iteration 2 tests to add players to team roster

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -53,6 +53,11 @@ describe Team do
         player_3 = Player.new("Alan McClennan", 750000, 48)
         player_4 = Player.new("Hamilton Porter", 100000, 12)
 
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
         expect(team.long_term_players).to eq([player_1, player_3])
     end
 
@@ -64,6 +69,11 @@ describe Team do
         player_3 = Player.new("Alan McClennan", 750000, 48)
         player_4 = Player.new("Hamilton Porter", 100000, 12)
 
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
         expect(team.short_term_players).to eq([player_2, player_4])
     end
 
@@ -74,6 +84,11 @@ describe Team do
         player_2 = Player.new("Kenny DeNunez", 500000, 24)
         player_3 = Player.new("Alan McClennan", 750000, 48)
         player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
 
         expect(team.total_value).to eq(85200000)
     end
