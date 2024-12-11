e8d22c3862d65f2c28a8b957ff45798889013311
Jaren Garman <jarengarman@protonmail.com>
2024-12-11 11:16:09 -0500

    feat: Add player class and initialize method

---
+++ lib/player.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Create a player with a name, monthly cost, and contract length in months
+class Player
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :total_cost
+
+  def initialize(name, monthly_cost, contract_length)
+    @first_name = name.split.first
+    @last_name = name.split.last
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+    @total_cost = monthly_cost * contract_length
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,39 +1,42 @@
+# frozen_string_literal: true
+
 require 'rspec'
+require_relative '../lib/player'
 
 RSpec.describe Player do
   it 'exists' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-    
-    expect(player).to be_a Player
+    player = described_class.new('Michael Palledorous', 1_000_000, 36)
+
+    expect(player).to be_a described_class
   end
 
   it 'has a first name' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
+    player = described_class.new('Michael Palledorous', 1_000_000, 36)
 
-    expect(player.first_name).to eq("Michael")
+    expect(player.first_name).to eq('Michael')
   end
 
   it 'has a last name' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
+    player = described_class.new('Michael Palledorous', 1_000_000, 36)
 
-    expect(player.last_name).to eq("Palledorous")
+    expect(player.last_name).to eq('Palledorous')
   end
 
   it 'has a monthly cost' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
+    player = described_class.new('Michael Palledorous', 1_000_000, 36)
 
-    expect(player.monthly_cost).to eq(1000000)
+    expect(player.monthly_cost).to eq(1_000_000)
   end
 
   it 'has a contract length' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
+    player = described_class.new('Michael Palledorous', 1_000_000, 36)
 
     expect(player.contract_length).to eq(36)
   end
 
   it 'has a total cost' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
+    player = described_class.new('Michael Palledorous', 1_000_000, 36)
 
-    expect(player.total_costg).to eq(36000000)
+    expect(player.total_cost).to eq(36_000_000)
   end
 end
