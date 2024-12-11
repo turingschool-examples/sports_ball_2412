d537f56de1e17ada34a4357ae27ef9f1b2ef5d83
Sebastian McKee <sebmcke@gmail.com>
2024-12-11 10:53:14 -0600

    Add iteration 4 tests to team spec class

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -92,4 +92,50 @@ describe Team do
 
         expect(team.total_value).to eq(85200000)
     end
+
+    it 'can get team deatils' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.deatils).to eq({"total_value" => 85200000, "player_count" => 4})
+    end
+
+    it 'can get the average cost of a player' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.average_cost_of_player).to eq("$21,300,000")
+    end
+
+    it 'can fetch the last name of every player' do
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+        team.add_player(player_3)
+        team.add_player(player_4)
+
+        expect(team.players_by_last_name).to eq("DeNunez, McClennan, Palledorous, Porter")
+    end
 end
