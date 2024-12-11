7dc0a90bf33f4b27a57fca67a0a60f7a97d5456d
Chase Phillips <chase.thomas.phillips@gmail.com>
2024-12-11 11:50:31 -0500

    finished plaer spec test and player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,22 @@
 class Player
-  attr_reader
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :total_cost
 
-  def initialize
+  def initialize(full_name, monthly_cost, contract_length)
+    @full_name = full_name
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
 
+
+    set_names
   end
-end
+
+  def set_names
+    name_parts = @full_name.split
+    @first_name = name_parts[0]
+    @last_name = name_parts[1]
+  end
+
+  def total_cost
+    @monthly_cost * @contract_length
+  end
+end  
