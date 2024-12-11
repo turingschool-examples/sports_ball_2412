6e297dee95ef803e839079cb116582a63ad05041
Jaren Garman <jarengarman@protonmail.com>
2024-12-11 11:33:22 -0500

    test: Add tests for long term and short term players as well as total value

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -35,7 +35,7 @@ describe Team do
     expect(team.player_count).to eq(0)
   end
 
-  it 'can add players' do # rubocop:disable RSpec/ExampleLength
+  it 'can add players' do
     team = described_class.new('Dodgers', 'Los Angeles')
     player1 = Player.new('Michael Palledorous', 1_000_000, 36)
     player2 = Player.new('Kenny DeNunez', 500_000, 24)
@@ -44,5 +44,50 @@ describe Team do
     team.add_player(player2)
 
     expect(team.player_count).to eq(2)
+  end
+
+  it 'can have long term players' do
+    team = described_class.new('Dodgers', 'Los Angeles')
+    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
+    player2 = Player.new('Kenny DeNunez', 500_000, 24)
+    player3 = Player.new('Alan McClennan', 750_000, 48)
+    player4 = Player.new('Hamilton Porter', 100_000, 12)
+
+    team.add_player(player1)
+    team.add_player(player2)
+    team.add_player(player3)
+    team.add_player(player4)
+
+    expect(team.long_term_players).to eq([player1, player3])
+  end
+
+  it 'can have short term players' do
+    team = described_class.new('Dodgers', 'Los Angeles')
+    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
+    player2 = Player.new('Kenny DeNunez', 500_000, 24)
+    player3 = Player.new('Alan McClennan', 750_000, 48)
+    player4 = Player.new('Hamilton Porter', 100_000, 12)
+
+    team.add_player(player1)
+    team.add_player(player2)
+    team.add_player(player3)
+    team.add_player(player4)
+
+    expect(team.short_term_players).to eq([player2, player4])
+  end
+
+  it 'can get total value' do
+    team = described_class.new('Dodgers', 'Los Angeles')
+    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
+    player2 = Player.new('Kenny DeNunez', 500_000, 24)
+    player3 = Player.new('Alan McClennan', 750_000, 48)
+    player4 = Player.new('Hamilton Porter', 100_000, 12)
+
+    team.add_player(player1)
+    team.add_player(player2)
+    team.add_player(player3)
+    team.add_player(player4)
+
+    expect(team.total_value).to eq(85_200_000)
   end
 end
