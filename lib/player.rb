dd36e64474cf26f7c9e85d97c58eaea71cda79d8
Donte Handy <donte.handy@gmail.com>
2024-12-11 12:43:32 -0400

    docs: it1/rspec test done

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,16 @@
+require 'rspec'
+require './lib/player'
+
 class Player
-    attr_reader :name, :position, :team
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length
 
-    def initialize(name, position, team)
-        @name = name
-        @position = position
-        @team = team
-    end
+  def initialize(name, monthly_cost, contract_length)
+    @first_name, @last_name = name.split
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
+  end
 
-    def display_info
-        "#{name} plays as a #{position} for #{team}."
-    end
+  def total_cost
+    @monthly_cost * @contract_length
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,45 +1,38 @@
-#player_spec.rb
-
-#this is to test the player class / file
-
-#in order to test the player class, we need to require the player class file
 require './lib/player'
+require 'rspec'
+
+#rspec spec/player_spec.rb 
+
 
 #this is the start of the test block
 RSpec.describe Player do #
   it 'exists' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-    
+    player = Player.new("Michael Palledorous", 1000000, 36)
     expect(player).to be_a Player
   end
 
-  xit 'has a first name' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
+  it 'has a first name' do
+    player = Player.new("Michael Palledorous", 1000000, 36)
     expect(player.first_name).to eq("Michael")
   end
 
-  xit 'has a last name' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
+  it 'has a last name' do
+    player = Player.new("Michael Palledorous", 1000000, 36)
     expect(player.last_name).to eq("Palledorous")
   end
 
-  xit 'has a monthly cost' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
+  it 'has a monthly cost' do
+    player = Player.new("Michael Palledorous", 1000000, 36)
     expect(player.monthly_cost).to eq(1000000)
   end
 
-  xit 'has a contract length' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
+  it 'has a contract length' do
+    player = Player.new("Michael Palledorous", 1000000, 36)
     expect(player.contract_length).to eq(36)
   end
 
-  xit 'has a total cost' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
-    expect(player.total_costg).to eq(36000000)
+  it 'has a total cost' do
+    player = Player.new("Michael Palledorous", 1000000, 36)
+    expect(player.total_cost).to eq(36000000)
   end
 end
