56753506fa98b8b6977a711c4fb036e59839cb49
Patrick Little <little.patrick@outlook.com>
2024-12-11 09:56:37 -0700

    adding team_spec

---
+++ spec/team_spec.rb
@@ -0,0 +1,64 @@
+require './lib/player.rb'
+require './lib/team.rb'
+require 'rspec'
+
+RSpec.describe Team do
+  before(:each) do
+    @team = Team.new("Dodgers", "Los Angeles")
+  end
+
+  it 'exists' do
+    expect(@team).to be_a Team
+  end
+
+  it 'has a roster' do
+    expect(@team.roster).to eq([])
+  end
+end
+
+RSpec.describe Team do
+  before(:each) do
+    @team = Team.new("Dodgers", "Los Angeles")
+    @player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    @player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+    @team.add_player(@player_1)
+    @team.add_player(@player_2)
+  end
+
+  it 'has a roster' do
+    expect(@team.roster).to eq([@player_1, @player_2])
+  end
+
+  it 'counts players' do
+    expect(@team.player_count).to eq(2)
+  end
+
+end
+
+RSpec.describe Team do
+  before(:each) do
+    @team = Team.new("Dodgers", "Los Angeles")
+
+    @player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+    @player_2 = Player.new("Kenny DeNunez", 500000, 24)
+    @player_3 = Player.new("Alan McClennan", 750000, 48)
+    @player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+    @team.add_player(@player_1)
+    @team.add_player(@player_2)
+    @team.add_player(@player_3)
+    @team.add_player(@player_4)
+  end
+  it 'has long term players' do
+    expect(@team.long_term_players).to eq([@player_1, @player_2])
+  end
+
+  it 'has short term players' do
+    expect(@team.short_term_players).to eq([@player_3, @player_4])
+  end
+
+  it 'has payroll' do
+    expect(@team.total_value).to eq(85200000)
+  end
+end
