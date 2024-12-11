88d41b98046e8d591e03189bd2dd9788fb67746d
Donte Handy <donte.handy@gmail.com>
2024-12-11 13:54:01 -0400

    better output in terminal

--- lib/player.rb
+++ lib/player.rb
@@ -2,22 +2,22 @@ require 'rspec' #connect to rspec gem
 require './lib/player' #makes the player class available to the test
 
 class Player
-  attr_reader :first_name, :last_name, :monthly_cost, :contract_length
+  attr_reader :first_name, :last_name, :contract, :contract_length
 
-  def initialize(name, monthly_cost, contract_length)
+  def initialize(name, contract, contract_length)
     @first_name, @last_name = name.split 
     #new instance of player class with 3 arguments
     #split method is used to split the name into first and last name
     #name is the argument passed to the initialize method
-    @monthly_cost = monthly_cost
+    @contract = contract
     @contract_length = contract_length
   end
 
   def total_cost
-    @monthly_cost * @contract_length
+    @contract * @contract_length
   end
 
-  def add_player(player)
-    @roster << player
-end
+  def to_s
+    "Name: #{@first_name} #{@last_name}, Contract: #{@contract}, Contract Length: #{@contract_length} months"
+  end
 end
