c913685fcd1c14f61da449dbd510c565e5fa2067
Will Fox <william.macdonald.fox@gmail.com>
2024-12-11 13:29:02 -0500

    test: complete all tests for iteration 3, tests passing, fixed indentation to align with my vscode extension indent-rainbow

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,39 +2,59 @@ require 'rspec'
 require './lib/player'
 
 RSpec.describe Player do
-  it 'exists' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-    
-    expect(player).to be_a Player
+  describe '#initialize' do
+    it 'exists' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
+
+      expect(player).to be_a(Player)
+    end
+
+    it 'has a first name' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
+
+      expect(player.first_name).to eq("Michael")
+    end
+
+    it 'has a last name' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
+
+      expect(player.last_name).to eq("Palledorous")
+    end
+
+    it 'has a monthly cost' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
+
+      expect(player.monthly_cost).to eq(1000000)
+    end
+
+    it 'has a contract length' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
+
+      expect(player.contract_length).to eq(36)
+    end
+
+    it 'has a default nickname set to nil' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
+
+      expect(player.nickname).to be(nil)
+    end
   end
 
-  it 'has a first name' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
+  describe '#total_cost' do
+    it 'calculates the total cost of the contract' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
 
-    expect(player.first_name).to eq("Michael")
+      expect(player.total_cost).to eq(36000000)
+    end
   end
 
-  it 'has a last name' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
+  describe '#set_nickname!' do
+    it 'can set and update a nickname' do
+      player = Player.new("Michael Palledorous", 1000000, 36)
 
-    expect(player.last_name).to eq("Palledorous")
-  end
+      player.set_nickname!("Squints")
 
-  it 'has a monthly cost' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
-    expect(player.monthly_cost).to eq(1000000)
-  end
-
-  it 'has a contract length' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
-    expect(player.contract_length).to eq(36)
-  end
-
-  it 'has a total cost' do
-    player = Player.new("Michael Palledorous" , 1000000, 36)
-
-    expect(player.total_cost).to eq(36000000)
+      expect(player.nickname).to eq("Squints")
+    end
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -6,72 +6,78 @@ RSpec.describe Team do
     before(:each) do 
         @team = Team.new("Dodgers", "Los Angeles") 
 
-        @player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        @player_1 = Player.new("Michael Palledorous", 1000000, 36)
         @player_2 = Player.new("Kenny DeNunez", 500000, 24)
         @player_3 = Player.new("Alan McClennan", 750000, 48)
         @player_4 = Player.new("Hamilton Porter", 100000, 12)
     end
 
-    it 'exists' do 
-        expect(@team).to be_a(Team)
+    describe '#initialize' do
+        it 'exists' do 
+            expect(@team).to be_a(Team)
+        end
+
+        it 'has a team name' do 
+            expect(@team.team_name).to eq("Dodgers")
+        end
+
+        it 'has a city' do 
+            expect(@team.city).to eq("Los Angeles")
+        end
+
+        it 'starts with an empty roster' do 
+            expect(@team.roster).to eq([])
+        end
     end
 
-    it 'has a team name' do 
-        expect(@team.team_name).to eq("Dodgers")
+    describe '#add_player' do
+        it 'can add multiple players to the roster' do 
+            @team.add_player(@player_1)
+            @team.add_player(@player_2)
+
+            expect(@team.roster).to eq([@player_1, @player_2])
+        end
     end
 
-    it 'has a city' do 
-        expect(@team.city).to eq("Los Angeles")
+    describe '#player_count' do
+        it 'counts the players in the roster' do 
+            @team.add_player(@player_1)
+            @team.add_player(@player_2)
+
+            expect(@team.player_count).to eq(2)
+        end
     end
 
-    it 'starts with an empty roster' do 
-        expect(@team.roster).to eq([])
+    describe '#long_term_players' do
+        it 'returns players with long-term contracts (contract length > 24 months)' do
+            @team.add_player(@player_1)
+            @team.add_player(@player_2)
+            @team.add_player(@player_3)
+            @team.add_player(@player_4)
+
+            expect(@team.long_term_players).to eq([@player_1, @player_3])
+        end
     end
 
-    it 'can add multiple players to the roster' do 
-        @team.add_player(@player_1)
-        @team.add_player(@player_2)
+    describe '#short_term_players' do
+            it 'returns players with short-term contracts (contract length <= 24 months)' do
+            @team.add_player(@player_1)
+            @team.add_player(@player_2)
+            @team.add_player(@player_3)
+            @team.add_player(@player_4)
 
-        expect(@team.roster).to eq([@player_1, @player_2])
+            expect(@team.short_term_players).to eq([@player_2, @player_4])
+        end
     end
 
-    it 'can count the players in the roster' do 
-        @team.add_player(@player_1)
-        @team.add_player(@player_2)
+    describe '#total_value' do
+        it 'calculates the total value of all player contracts' do
+            @team.add_player(@player_1)
+            @team.add_player(@player_2)
+            @team.add_player(@player_3)
+            @team.add_player(@player_4)
 
-        expect(@team.player_count).to eq(2)
-    end
-
-    it 'records and returns the players with long term contracts' do
-        @team.add_player(@player_1)
-        @team.add_player(@player_2)
-        @team.add_player(@player_3)
-        @team.add_player(@player_4)
-
-        expect(@team.roster).to eq([@player_1, @player_2, @player_3, @player_4])
-        expect(@team.player_count).to eq(4)
-        expect(@team.long_term_players).to eq([@player_1, @player_3])
-    end
-
-    it 'records and returns the players with short term contracts' do
-        @team.add_player(@player_1)
-        @team.add_player(@player_2)
-        @team.add_player(@player_3)
-        @team.add_player(@player_4)
-
-        expect(@team.roster).to eq([@player_1, @player_2, @player_3, @player_4])
-        expect(@team.player_count).to eq(4)
-        expect(@team.short_term_players).to eq([@player_2, @player_4])
-    end
-
-    it 'calculates the teams total player value by adding the total contract costs' do
-        @team.add_player(@player_1)
-        @team.add_player(@player_2)
-        @team.add_player(@player_3)
-        @team.add_player(@player_4)
-
-        expect(@team.roster).to eq([@player_1, @player_2, @player_3, @player_4])
-        expect(@team.player_count).to eq(4)
-        expect(@team.total_value).to eq(85200000)
+            expect(@team.total_value).to eq(85200000)
+        end
     end
 end
