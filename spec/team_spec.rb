5b0e06bd740b655c6032a96459a3f522eb6b0fc3
Will Fox <william.macdonald.fox@gmail.com>
2024-12-11 12:49:24 -0500

    test: complete total_value test

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -63,4 +63,15 @@ RSpec.describe Team do
         expect(@team.player_count).to eq(4)
         expect(@team.short_term_players).to eq([@player_2, @player_4])
     end
+
+    it 'calculates the teams total player value by adding the total contract costs' do
+        @team.add_player(@player_1)
+        @team.add_player(@player_2)
+        @team.add_player(@player_3)
+        @team.add_player(@player_4)
+
+        expect(@team.roster).to eq([@player_1, @player_2, @player_3, @player_4])
+        expect(@team.player_count).to eq(4)
+        expect(@team.total_value).to eq(85200000)
+    end
 end
