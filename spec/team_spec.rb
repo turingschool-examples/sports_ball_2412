96b4336a7c1f0c456a9b0d5e6fd285579b29bdda
Jaren Garman <jarengarman@protonmail.com>
2024-12-11 13:10:35 -0500

    test: Refactor team_spec to comply with RSpec style guide

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -5,134 +5,191 @@ require_relative '../lib/team'
 require_relative '../lib/player'
 
 describe Team do
-  it 'exists' do
-    team = described_class.new('Dodgers', 'Los Angeles')
+  let(:team) { described_class.new('Dodgers', 'Los Angeles') }
+  let(:first_player) { Player.new('Michael Palledorous', 1_000_000, 36) }
+  let(:second_player) { Player.new('Kenny DeNunez', 500_000, 24) }
+  let(:third_player) { Player.new('Alan McClennan', 750_000, 48) }
+  let(:fourth_player) { Player.new('Hamilton Porter', 100_000, 12) }
 
-    expect(team).to be_an_instance_of(described_class)
+  describe '#initialize' do
+    subject(:team) { described_class.new('Dodgers', 'Los Angeles') }
+
+    it { is_expected.not_to be_nil }
+    it { is_expected.to be_an_instance_of described_class }
+
+    describe '#name' do
+      subject(:name) { team.name }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns name' do
+        expect(name).to eq('Dodgers')
+      end
+    end
+
+    describe '#city' do
+      subject(:city) { team.city }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns city' do
+        expect(city).to eq('Los Angeles')
+      end
+    end
   end
 
-  it 'has a name' do
-    team = described_class.new('Dodgers', 'Los Angeles')
+  context 'when roster is empty' do
+    describe '#roster' do
+      subject(:roster) { team.roster }
 
-    expect(team.name).to eq('Dodgers')
+      it { is_expected.not_to be_nil }
+
+      it 'returns empty array' do
+        expect(roster).to eq([])
+      end
+    end
+
+    describe '#player_count' do
+      subject(:player_count) { team.player_count }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns 0' do
+        expect(player_count).to eq(0)
+      end
+    end
+
+    describe '#long_term_players' do
+      subject(:long_term_players) { team.long_term_players }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns empty array' do
+        expect(long_term_players).to eq([])
+      end
+    end
+
+    describe '#short_term_players' do
+      subject(:short_term_players) { team.short_term_players }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns empty array' do
+        expect(short_term_players).to eq([])
+      end
+    end
+
+    describe '#total_value' do
+      subject(:total_value) { team.total_value }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns 0' do
+        expect(total_value).to eq(0)
+      end
+    end
+
+    describe '#details' do
+      subject(:details) { team.details }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns details as hash with values of 0' do
+        expect(details).to eq({ 'total_value' => 0, 'player_count' => 0 })
+      end
+    end
+
+    describe '#average_cost_of_player' do
+      subject(:average_cost_of_player) { team.average_cost_of_player }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns "$0"' do
+        expect(team.average_cost_of_player).to eq('$0')
+      end
+    end
+
+    describe '#players_by_last_name' do
+      subject(:players_by_last_name) { team.players_by_last_name }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns empty array' do
+        expect(players_by_last_name).to eq([])
+      end
+    end
   end
 
-  it 'has a city' do
-    team = described_class.new('Dodgers', 'Los Angeles')
+  context 'when roster is full' do
+    before do
+      team.add_player(first_player)
+      team.add_player(second_player)
+      team.add_player(third_player)
+      team.add_player(fourth_player)
+    end
 
-    expect(team.city).to eq('Los Angeles')
-  end
+    describe '#roster' do
+      subject(:roster) { team.roster }
 
-  it 'has a roster' do
-    team = described_class.new('Dodgers', 'Los Angeles')
+      it 'returns array of players' do
+        expect(roster).to eq([first_player, second_player, third_player, fourth_player])
+      end
+    end
 
-    expect(team.roster).to eq([])
-  end
+    describe '#player_count' do
+      subject(:player_count) { team.player_count }
 
-  it 'can count players' do
-    team = described_class.new('Dodgers', 'Los Angeles')
+      it 'returns amount of players as integer' do
+        expect(player_count).to eq(4)
+      end
+    end
 
-    expect(team.player_count).to eq(0)
-  end
+    describe '#long_term_players' do
+      subject(:long_term_players) { team.long_term_players }
 
-  it 'can add players' do
-    team = described_class.new('Dodgers', 'Los Angeles')
-    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
-    player2 = Player.new('Kenny DeNunez', 500_000, 24)
+      it 'returns array of long term players' do
+        expect(long_term_players).to eq([first_player, third_player])
+      end
+    end
 
-    team.add_player(player1)
-    team.add_player(player2)
+    describe '#short_term_players' do
+      subject(:short_term_players) { team.short_term_players }
 
-    expect(team.player_count).to eq(2)
-  end
+      it 'returns array of short term players' do
+        expect(short_term_players).to eq([second_player, fourth_player])
+      end
+    end
 
-  it 'can have long term players' do
-    team = described_class.new('Dodgers', 'Los Angeles')
-    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
-    player2 = Player.new('Kenny DeNunez', 500_000, 24)
-    player3 = Player.new('Alan McClennan', 750_000, 48)
-    player4 = Player.new('Hamilton Porter', 100_000, 12)
+    describe '#total_value' do
+      subject(:total_value) { team.total_value }
 
-    team.add_player(player1)
-    team.add_player(player2)
-    team.add_player(player3)
-    team.add_player(player4)
+      it 'returns total value as integer' do
+        expect(total_value).to eq(85_200_000)
+      end
+    end
 
-    expect(team.long_term_players).to eq([player1, player3])
-  end
+    describe '#details' do
+      subject(:details) { team.details }
 
-  it 'can have short term players' do
-    team = described_class.new('Dodgers', 'Los Angeles')
-    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
-    player2 = Player.new('Kenny DeNunez', 500_000, 24)
-    player3 = Player.new('Alan McClennan', 750_000, 48)
-    player4 = Player.new('Hamilton Porter', 100_000, 12)
+      it 'returns details as hash' do
+        expect(details).to eq({ 'total_value' => 85_200_000, 'player_count' => 4 })
+      end
+    end
 
-    team.add_player(player1)
-    team.add_player(player2)
-    team.add_player(player3)
-    team.add_player(player4)
+    describe '#average_cost_of_player' do
+      subject(:average_cost_of_player) { team.average_cost_of_player }
 
-    expect(team.short_term_players).to eq([player2, player4])
-  end
+      it 'returns average cost of player as properly formatted string' do
+        expect(team.average_cost_of_player).to eq('$21,300,000')
+      end
+    end
 
-  it 'can get total value' do
-    team = described_class.new('Dodgers', 'Los Angeles')
-    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
-    player2 = Player.new('Kenny DeNunez', 500_000, 24)
-    player3 = Player.new('Alan McClennan', 750_000, 48)
-    player4 = Player.new('Hamilton Porter', 100_000, 12)
+    describe '#players_by_last_name' do
+      subject(:players_by_last_name) { team.players_by_last_name }
 
-    team.add_player(player1)
-    team.add_player(player2)
-    team.add_player(player3)
-    team.add_player(player4)
-
-    expect(team.total_value).to eq(85_200_000)
-  end
-
-  it 'can get details' do
-    team = described_class.new('Dodgers', 'Los Angeles')
-    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
-    player2 = Player.new('Kenny DeNunez', 500_000, 24)
-    player3 = Player.new('Alan McClennan', 750_000, 48)
-    player4 = Player.new('Hamilton Porter', 100_000, 12)
-
-    team.add_player(player1)
-    team.add_player(player2)
-    team.add_player(player3)
-    team.add_player(player4)
-
-    expect(team.details).to eq({ 'total_value' => 85_200_000, 'player_count' => 4 })
-  end
-
-  it 'can get average cost of player' do
-    team = described_class.new('Dodgers', 'Los Angeles')
-    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
-    player2 = Player.new('Kenny DeNunez', 500_000, 24)
-    player3 = Player.new('Alan McClennan', 750_000, 48)
-    player4 = Player.new('Hamilton Porter', 100_000, 12)
-
-    team.add_player(player1)
-    team.add_player(player2)
-    team.add_player(player3)
-    team.add_player(player4)
-
-    expect(team.average_cost_of_player).to eq('$21,300,000')
-  end
-
-  it 'can sort by last name' do
-    team = described_class.new('Dodgers', 'Los Angeles')
-    player1 = Player.new('Michael Palledorous', 1_000_000, 36)
-    player2 = Player.new('Kenny DeNunez', 500_000, 24)
-    player3 = Player.new('Alan McClennan', 750_000, 48)
-    player4 = Player.new('Hamilton Porter', 100_000, 12)
-
-    team.add_player(player1)
-    team.add_player(player2)
-    team.add_player(player3)
-    team.add_player(player4)
-
-    expect(team.players_by_last_name).to eq([player4, player1, player3, player2])
+      it 'returns array of players sorted by last name' do
+        expect(team.players_by_last_name).to eq([fourth_player, first_player, third_player, second_player])
+      end
+    end
   end
 end
