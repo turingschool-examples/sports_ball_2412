b7a1df828afc611b2c1f6725e2a107f6a27f068e
Jaren Garman <jarengarman@protonmail.com>
2024-12-11 11:40:16 -0500

    feat: Add nickname for Player

--- lib/player.rb
+++ lib/player.rb
@@ -2,7 +2,7 @@
 
 # Create a player with a name, monthly cost, and contract length in months
 class Player
-  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :total_cost
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length, :total_cost, :nickname
 
   def initialize(name, monthly_cost, contract_length)
     @first_name = name.split.first
@@ -10,5 +10,9 @@ class Player
     @monthly_cost = monthly_cost
     @contract_length = contract_length
     @total_cost = monthly_cost * contract_length
+  end
+
+  def set_nickname!(nickname)
+    @nickname = nickname
   end
 end
