803fceae04fa32392293d66515f3d8e7a74cd977
Jacob Inman <jrinman6295@gmail.com>
2024-12-11 09:32:14 -0800

    added the method to give nicknames to players

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,11 @@
 class Player
-    attr_reader :name, :monthly_cost, :contract_length
+    attr_reader :name, :monthly_cost, :contract_length, :nickname
 
     def initialize(name, monthly_cost, contract_length)
         @name = name
         @monthly_cost = monthly_cost
         @contract_length = contract_length
+        @nickname = ()
     end
 
     def first_name
@@ -25,5 +26,9 @@ class Player
 
     def total_cost
         @monthly_cost * @contract_length
+    end
+
+    def set_nickname(nickname)
+        @nickname = nickname
     end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,14 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+  it 'can give a player a nickname' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    expect(player.nickname).to eq nil
+
+    player.set_nickname("Squints")
+
+    expect(player.nickname).to eq("Squints")
+  end
 end
