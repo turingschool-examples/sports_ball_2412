41d013222f47e654bbbf9315736c0469e211270b
Jaren Garman <jarengarman@protonmail.com>
2024-12-11 12:17:36 -0500

    test: Refactor player_spec to comply with RSpec style guide

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,53 +4,76 @@ require 'rspec'
 require_relative '../lib/player'
 
 describe Player do
-  it 'exists' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
+  subject(:player) { described_class.new('Michael Palledorous', 1_000_000, 36) }
 
-    expect(player).to be_a described_class
+  describe '#initialize' do
+    it { is_expected.not_to be_nil }
+    it { is_expected.to be_an_instance_of described_class }
+
+    describe '#first_name' do
+      subject(:first_name) { player.first_name }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns first name' do
+        expect(player.first_name).to eq('Michael')
+      end
+    end
+
+    describe '#last_name' do
+      subject(:last_name) { player.last_name }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns last name' do
+        expect(player.last_name).to eq('Palledorous')
+      end
+    end
+
+    describe '#monthly_cost' do
+      subject(:monthly_cost) { player.monthly_cost }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns monthly cost' do
+        expect(player.monthly_cost).to eq(1_000_000)
+      end
+    end
+
+    describe '#contract_length' do
+      subject(:contract_length) { player.contract_length }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns last name' do
+        expect(player.contract_length).to eq(36)
+      end
+    end
+
+    describe '#total_cost' do
+      subject(:total_cost) { player.total_cost }
+
+      it { is_expected.not_to be_nil }
+
+      it 'returns last name' do
+        expect(player.total_cost).to eq(36_000_000)
+      end
+    end
   end
 
-  it 'has a first name' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
+  describe '#nickname' do
+    subject(:nickname) { player.nickname }
 
-    expect(player.first_name).to eq('Michael')
-  end
+    context 'when nickname has not been set' do
+      it { is_expected.to be_nil }
+    end
 
-  it 'has a last name' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
+    context 'when nickname has been set' do
+      it 'returns nickname' do
+        player.set_nickname!('Squints')
 
-    expect(player.last_name).to eq('Palledorous')
-  end
-
-  it 'has a monthly cost' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
-
-    expect(player.monthly_cost).to eq(1_000_000)
-  end
-
-  it 'has a contract length' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
-
-    expect(player.contract_length).to eq(36)
-  end
-
-  it 'has a total cost' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
-
-    expect(player.total_cost).to eq(36_000_000)
-  end
-
-  it 'has no nickname by default' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
-
-    expect(player.nickname).to be_nil
-  end
-
-  it 'can add a nickname' do
-    player = described_class.new('Michael Palledorous', 1_000_000, 36)
-
-    player.set_nickname!('Squints')
-
-    expect(player.nickname).to eq('Squints')
+        expect(player.nickname).to eq('Squints')
+      end
+    end
   end
 end
