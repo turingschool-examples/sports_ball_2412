01e036d84e6b816c01263574ee278ee7788599b3
Sebastian McKee <sebmcke@gmail.com>
2024-12-11 10:26:17 -0600

    Add iteration 2 tests

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -8,4 +8,40 @@ describe Team do
 
         expect(team).to be_a(Team)
     end
+
+    it 'has a roster' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        expect(team.roster).to eq([])
+    end
+
+    it 'has a player cound' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        expect(team.player_count).to eq(0)
+    end
+
+    it 'can hold players in a roster' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+
+        expect(team.roster).to eq([player_1, player_2])
+    end
+
+    it 'has an accurate player count' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+
+        team.add_player(player_1)
+        team.add_player(player_2)
+
+        expect(team.player_count).to eq(2)
+    end
 end
