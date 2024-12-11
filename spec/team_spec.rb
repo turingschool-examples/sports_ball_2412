61b1c266b1817602900ffcb4554a8c3fc6b1381a
Sebastian McKee <sebmcke@gmail.com>
2024-12-11 10:33:27 -0600

    Add iteration 3 tests to team spec class

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -44,4 +44,37 @@ describe Team do
 
         expect(team.player_count).to eq(2)
     end
+
+    it 'can fetch long term players' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        expect(team.long_term_players).to eq([player_1, player_3])
+    end
+
+    it 'can fetch short term players' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        expect(team.short_term_players).to eq([player_2, player_4])
+    end
+
+    it 'can fetch the monthly payroll of all players' do
+        team = Team.new("Dodgers", "Los Angeles")
+
+        player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+        player_2 = Player.new("Kenny DeNunez", 500000, 24)
+        player_3 = Player.new("Alan McClennan", 750000, 48)
+        player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+        expect(team.total_value).to eq(85200000)
+    end
 end
