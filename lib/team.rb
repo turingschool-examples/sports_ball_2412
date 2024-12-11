7407696c3e5db376fa7ac0129d13f78fff1cb4f8
Coral <coralruschak@gmail.com>
2024-12-11 13:53:56 -0500

    iteration 3: team updates pass testing

--- lib/team.rb
+++ lib/team.rb
@@ -14,4 +14,32 @@ class Team
   def add_player(player)
     @roster << player
   end
+
+  def long_term_players
+    long_term = []
+    @roster.each do |player|
+      if player.contract_length > 24
+        long_term << player
+      end
+    end
+    return long_term
+  end
+
+  def short_term_players
+    short_term = []
+    @roster.each do |player|
+      if player.contract_length <= 24
+        short_term << player
+      end
+    end
+    return short_term
+  end
+
+  def total_value
+    payroll = 0
+    @roster.each do |player|
+      payroll += player.total_cost
+    end
+    return payroll
+  end
 end
